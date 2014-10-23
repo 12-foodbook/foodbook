378b92ee53da9da2a4439fcd18f68b26800f29ce
Sarach Tuomchomtam <icybertize@gmail.com>
2014-10-23 15:43:01 +0700

    Set DataSource to application scope

--- src/th/ac/kmitl/it/foodbook/listeners/InitializeListener.java
+++ src/th/ac/kmitl/it/foodbook/listeners/InitializeListener.java
@@ -1,6 +1,7 @@
 package th.ac.kmitl.it.foodbook.listeners;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -10,20 +11,23 @@ import th.ac.kmitl.it.foodbook.Foodbook;
 
 @WebListener
 public class InitializeListener implements ServletContextListener {
-	
-	@Resource(name="jdbc/foodbook_development")
+
+	@Resource(name = "jdbc/foodbook_development")
 	private DataSource ds;
 
-    public InitializeListener() {
-	
-    }
+	public InitializeListener() {
 
-    public void contextInitialized(ServletContextEvent sce)  { 
-         sce.getServletContext().setAttribute("siteTitle", Foodbook.NANE);
-    }
+	}
 
-    public void contextDestroyed(ServletContextEvent sce)  { 
-         
-    }
-	
+	public void contextInitialized(ServletContextEvent sce) {
+		ServletContext context = sce.getServletContext();
+		
+		context.setAttribute("ds", ds);
+		context.setAttribute("siteTitle", Foodbook.NANE);
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+
+	}
+
 }
