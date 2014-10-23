cd1e5c748f774ad8c03f7c8e4a5815c5c3c9d00d
Sarach Tuomchomtam <icybertize@gmail.com>
2014-10-23 16:37:12 +0700

    Add code to /users/create

--- src/th/ac/kmitl/it/foodbook/servlets/users/CreateServlet.java
+++ src/th/ac/kmitl/it/foodbook/servlets/users/CreateServlet.java
@@ -1,12 +1,19 @@
 package th.ac.kmitl.it.foodbook.servlets.users;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import th.ac.kmitl.it.foodbook.Foodbook;
+import th.ac.kmitl.it.foodbook.beans.User;
+import th.ac.kmitl.it.foodbook.daos.UsersDAO;
 
 @WebServlet("/users/create")
 public class CreateServlet extends HttpServlet {
@@ -16,12 +23,53 @@ public class CreateServlet extends HttpServlet {
     	super();
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String confirmPassword = request.getParameter("confirm_password");
+		
+		if (!password.equals(confirmPassword)) {
+			return;
+		}
+		
+		DataSource ds = (DataSource) request.getServletContext().getAttribute("ds");
+		
+		boolean isSuccess = false;
     	
+    	try {
+			Connection conn = ds.getConnection();
+			UsersDAO usersDAO = new UsersDAO(conn);
+			
+			User user = new User();
+			
+			user.setUsername(username);
+			
+			byte[] saltBytes = Foodbook.getSalt();
+			String salt = Foodbook.bytesToString(saltBytes);
+			byte[] hashedPasswordBytes = Foodbook.hashPassword(password, saltBytes);
+			String hashedPassword = Foodbook.bytesToString(hashedPasswordBytes);
+			
+			user.setHashed_password(hashedPassword);
+			user.setSalt(salt);
+			
+			isSuccess = usersDAO.create(user);
+			
+	    	conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+			response.sendError(500);
+		}
+    	
+    	if (isSuccess) {
+    		
+    	} else {
+    		
+    	}
     }
 
 }
