5490c6c41be50bec0276552f30f2e7411b8cf603
Sarach Tuomchomtam <icybertize@gmail.com>
2014-10-24 10:33:54 +0700

    Finish user authentication

--- src/th/ac/kmitl/it/foodbook/servlets/users/AuthenticateServlet.java
+++ src/th/ac/kmitl/it/foodbook/servlets/users/AuthenticateServlet.java
@@ -1,12 +1,19 @@
 package th.ac.kmitl.it.foodbook.servlets.users;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import th.ac.kmitl.it.foodbook.beans.User;
+import th.ac.kmitl.it.foodbook.daos.UsersDAO;
 
 @WebServlet("/users/authenticate")
 public class AuthenticateServlet extends HttpServlet {
@@ -21,7 +28,33 @@ public class AuthenticateServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String username = request.getParameter("username");
+    	String password = request.getParameter("password");
     	
+    	DataSource ds = (DataSource) request.getServletContext().getAttribute("ds");
+		
+		User user = null;
+		
+		try {
+			Connection conn = ds.getConnection();
+			UsersDAO usersDAO = new UsersDAO(conn);
+			
+			user = usersDAO.authenticate(username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+			response.sendError(500);
+		}
+		
+		HttpSession session = request.getSession();
+		
+		if (user != null) {
+			session.setAttribute("alert", "/users/authenticate success");
+			response.sendRedirect("/");
+		} else {
+			session.setAttribute("alert", "/users/authenticate not success");
+			request.getRequestDispatcher("/WEB-INF/views/users/authenticate.jsp").include(request, response);
+		}
     }
 
 }
