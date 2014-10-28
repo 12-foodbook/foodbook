ad224d5672d7966a5070bde736034c6567064d1a
Sarach Tuomchomtam <icybertize@gmail.com>
2014-10-28 19:56:26 +0700

    Create recipe category bean

---
+++ src/th/ac/kmitl/it/foodbook/beans/RecipeCategory.java
@@ -0,0 +1,24 @@
+package th.ac.kmitl.it.foodbook.beans;
+
+public class RecipeCategory {
+	
+	private long recipe_category_id;
+	private String name;
+	
+	public long getRecipe_category_id() {
+		return recipe_category_id;
+	}
+	
+	public void setRecipe_category_id(long recipe_category_id) {
+		this.recipe_category_id = recipe_category_id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
