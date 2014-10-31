7f6529764e613c300718318062225cc2221aa231
Sarach Tuomchomtam <icybertize@gmail.com>
2014-10-31 15:10:16 +0700

    Create ingredient category bean

---
+++ src/th/ac/kmitl/it/foodbook/beans/IngredientCategory.java
@@ -0,0 +1,24 @@
+package th.ac.kmitl.it.foodbook.beans;
+
+public class IngredientCategory {
+	
+	private long ingredient_category_id;
+	private String name;
+	
+	public long getIngredient_category_id() {
+		return ingredient_category_id;
+	}
+	
+	public void setIngredient_category_id(long ingredient_category_id) {
+		this.ingredient_category_id = ingredient_category_id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
