bcd7a31dfe2a65f90817be366df89ae1565c81b9
Sarach Tuomchomtam <icybertize@gmail.com>
2014-11-09 18:50:17 +0700

    Remove tests in methods

--- src/th/ac/kmitl/it/foodbook/servlets/recipes/SearchByIngredientServlet.java
+++ src/th/ac/kmitl/it/foodbook/servlets/recipes/SearchByIngredientServlet.java
@@ -31,7 +31,7 @@ public class SearchByIngredientServlet extends HttpServlet {
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
