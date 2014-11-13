5fc02f3d6e066741afce34f892db8aee46bcba3f
Minimi <minimi-moszii@live.com>
2014-11-13 22:08:37 +0700

    Create recipe categories IndexServlet

---
+++ src/th/ac/kmitl/it/foodbook/servlets/recipes/categories/IndexServlet.java
@@ -0,0 +1,55 @@
+package th.ac.kmitl.it.foodbook.servlets.recipes.categories;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import th.ac.kmitl.it.foodbook.beans.RecipeCategory;
+import th.ac.kmitl.it.foodbook.daos.RecipeCategoriesDAO;
+
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public IndexServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		List<RecipeCategory> recipeCategories = null;
+
+		DataSource ds = (DataSource) request.getServletContext().getAttribute(
+				"ds");
+
+		try {
+			Connection conn = ds.getConnection();
+			RecipeCategoriesDAO recipeCategoriesDAO = new RecipeCategoriesDAO(
+					conn);
+
+			recipeCategories = recipeCategoriesDAO.findAll();
+
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			response.sendError(500);
+		}
+
+		request.setAttribute("recipeCategories", recipeCategories);
+
+		request.getRequestDispatcher(
+				"/WEB-INF/views/ingredients/categories/index.jsp").include(
+				request, response);
+	}
+
+}
