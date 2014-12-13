74e384bc7f0989a9b99fa41eb5e47dde44e271ef
Minimi <minimi-moszii@live.com>
2014-12-13 09:49:14 +0700

    Create removeAllRecipeFromSteps Function

--- src/th/ac/kmitl/it/foodbook/daos/RecipesDAO.java
+++ src/th/ac/kmitl/it/foodbook/daos/RecipesDAO.java
@@ -158,6 +158,17 @@ public class RecipesDAO extends AbstractDAO {
         return rowCount == 1;
     }
     
+    public boolean removeAllRecipeFromSteps(long recipeId) throws SQLException {
+        String sql = "DELETE FROM recipe_steps WHERE recipe_id = ?";
+        PreparedStatement stm = conn.prepareStatement(sql);
+        
+        stm.setLong(1, recipeId);
+        
+        int rowCount = stm.executeUpdate();
+        
+        return rowCount == 1;
+    }
+    
     public boolean update(Recipe recipe) throws SQLException {
         String sql = "UPDATE recipes SET name = ?, video_url = ? WHERE recipe_id = ?";
         PreparedStatement stm = conn.prepareStatement(sql);
