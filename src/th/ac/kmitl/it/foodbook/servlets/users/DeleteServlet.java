cb7d864fece5d91f3dd09fe6532429433f81fe0b
Minimi <minimi-moszii@live.com>
2014-12-15 13:44:04 +0700

    Remove unuse import

--- src/th/ac/kmitl/it/foodbook/servlets/users/DeleteServlet.java
+++ src/th/ac/kmitl/it/foodbook/servlets/users/DeleteServlet.java
@@ -12,9 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
-import th.ac.kmitl.it.foodbook.beans.RecipeCategory;
-import th.ac.kmitl.it.foodbook.daos.RecipeCategoriesDAO;
-import th.ac.kmitl.it.foodbook.daos.RecipesDAO;
 import th.ac.kmitl.it.foodbook.daos.UsersDAO;
 import th.ac.kmitl.it.foodbook.utils.Alert;
 import th.ac.kmitl.it.foodbook.utils.Alert.AlertTypes;
