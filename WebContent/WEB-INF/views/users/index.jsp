f3532719143786158a1e300a1e4f89b6ab13b260
Minimi <minimi-moszii@live.com>
2014-12-15 13:35:21 +0700

    Create user index page

---
+++ WebContent/WEB-INF/views/users/index.jsp
@@ -0,0 +1,58 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+
+<c:set var="pageTitle" value="Categories / Index" scope="application" />
+<jsp:include page="/WEB-INF/views/layouts/header.jsp" />
+
+<div class='container'>
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1"><!-- set table -->
+	
+		<form action='/users/delete' method="post">
+			<table id="cateTable" class="table table-striped"><!-- recipe category -->
+				<thead><td align="center">#</td><td align="center">ผู้ใช้</td><td align="center">ลบผู้ใช้</td><td></td></thead>
+				<c:forEach var="i" begin="0" end="${fn:length(users)-1}">
+					
+					<tr>
+						<td align="center" >${i+1}</td>
+						<td align="center" id='cateValue_${users[i].username}' value="${users[i].username}">${users[i].username}</td>
+						<td align="center"><input id='user_id' name="user_id" type="checkbox" value="${users[i].user_id}" /></td>
+						<td></td>
+					</tr>	
+				</c:forEach>
+			</table>
+			
+			
+			
+			<!-- button delete -->
+			<button type="button" class="btn btn-danger col-md-2" data-toggle="modal" data-target="#myModal">ลบผู้ใช้</button>
+			<!-- Modal -->
+			<div class="modal fade" id="myModal" tabindex="-1" role="dialog"aria-labelledby="myModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal">
+									<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+								</button>
+								<h4 class="modal-title" id="myModalLabel">ลบผู้ใช้</h4>
+							</div>
+							<div class="modal-body">ต้องการลบผู้ใช้หรือไม่?</div>
+							<div class="modal-footer">
+							<button type="button" class="btn btn-default"data-dismiss="modal">ยกเลิก</button>
+							<input onclick='sendValue()' type="submit" class="btn btn-danger" value='ลบ'>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+		
+		<!-- button add -->
+		
+			
+			
+		</div>
+	</div>
+</div>	
+<jsp:include page="/WEB-INF/views/layouts/footer.jsp" />
--- src/th/ac/kmitl/it/foodbook/servlets/users/DeleteServlet.java
+++ src/th/ac/kmitl/it/foodbook/servlets/users/DeleteServlet.java
@@ -9,9 +9,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
+import th.ac.kmitl.it.foodbook.beans.RecipeCategory;
+import th.ac.kmitl.it.foodbook.daos.RecipeCategoriesDAO;
+import th.ac.kmitl.it.foodbook.daos.RecipesDAO;
 import th.ac.kmitl.it.foodbook.daos.UsersDAO;
+import th.ac.kmitl.it.foodbook.utils.Alert;
+import th.ac.kmitl.it.foodbook.utils.Alert.AlertTypes;
 
 @WebServlet("/users/delete")
 public class DeleteServlet extends HttpServlet {
@@ -23,21 +29,41 @@ public class DeleteServlet extends HttpServlet {
     }
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String userIdString = request.getParameter("user_id");
-        long userId = Long.parseLong(userIdString);
+        String[] userIdsString = request.getParameterValues("user_id");
+        
+        boolean isSuccess = false;
         
         DataSource ds = (DataSource) request.getServletContext().getAttribute("ds");
+        
+        HttpSession session = request.getSession();
         
         try {
             Connection conn = ds.getConnection();
             
-            UsersDAO usersDAO = new UsersDAO(conn);
-            usersDAO.delete(userId);
+            if (userIdsString == null) {
+                response.sendRedirect("/users/index");
+                return;
+            }
+            
+            for (String userIdString : userIdsString) {
+                long userId = Long.parseLong(userIdString);
+                
+                UsersDAO usersDAO = new UsersDAO(conn);
+                isSuccess = usersDAO.delete(userId);
+            }
             
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
             response.sendError(500);
+        }
+        
+        if (isSuccess) {
+            session.setAttribute("alert", new Alert(AlertTypes.SUCCESS, "Deleted Successfully!"));
+            response.sendRedirect("/users/index");
+        } else {
+            session.setAttribute("alert", new Alert(AlertTypes.DANGER, "Deleted Unsuccessfully!"));
+            response.sendRedirect("/users/index");
         }
     }
     
